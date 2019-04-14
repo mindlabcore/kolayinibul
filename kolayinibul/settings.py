1d4ea3f1692d1ba982fff95f989943f8e3491b5b
MindLab Core <mindlabcore@gmail.com>
2019-04-15 00:37:10 +0300

    Advert deleted.

--- kolayinibul/settings.py
+++ kolayinibul/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '1pvy#$=1f#%rq3*3wp!+g93hsxa*9h^hsdt&iq-&sejjgd^k+m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.kolayinibul.com', '127.0.0.1']
+ALLOWED_HOSTS = ['www.kolayinibul.com', '127.0.0.1', '127.0.0.2']
 
 # Application definition
 
